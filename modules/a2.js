9466459aa8ed617b4b20c54690e7b3dfe4edbb6b
himanshu850 <himanshupparmar824@gmail.com>
2024-05-27 11:57:55 -0400

    second commit

---
+++ modules/a2.js
@@ -0,0 +1,34 @@
+/*********************************************************************************
+*  WEB700 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Himanshu Parmar Student ID: 146708235 Date: 5/27/2024
+*
+********************************************************************************/ 
+
+const collegeData = require('.\modules\collegeData.js');
+
+collegeData.initialize()
+    .then(() => {
+        console.log("Data successfully initialized");
+
+        return collegeData.getAllStudents();
+    })
+    .then(students => {
+        console.log(`Successfully retrieved ${students.length} students`);
+        
+        return collegeData.getCourses();
+    })
+    .then(courses => {
+        console.log(`Successfully retrieved ${courses.length} courses`);
+
+        return collegeData.getTAs();
+    })
+    .then(TAs => {
+        console.log(`Successfully retrieved ${TAs.length} TAs`);
+    })
+    .catch(err => {
+        console.log(err);
+    });
